9b9e88c892772395e16413978c56402041b19624
Vitalij Ryndin <crashmax2015@mail.ru>
2021-06-13 15:39:29 +0800

    @babel/preset-env targets

--- webpack.config.ts
+++ webpack.config.ts
@@ -34,9 +34,16 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    esmodules: true
+                  }
+                }
+              ]
+            ]
           },
           {
             loader: 'ts-loader'
